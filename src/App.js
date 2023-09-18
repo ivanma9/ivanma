59bb79df638c712107e57856950deb62064ee42c
Ivan <ivanma819@gmail.com>
2023-09-17 22:53:41 -0700

    Add css to navbar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
-import Layout from "antd";
+import { Layout } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import "./styles/Navbar.css";
+
+const { Header, Content, Footer, Sider } = Layout;
 
 
 function App() {
-	return (
+	const [expandNavbar, setExpandedNavbar] = useState(false)
 
+	return (
 		<div className="App">
-			<div>
-				<div className="navbar">
-					<div className="toggleButton">
-						<button></button>
-					</div>
-					<div className="links">
-					
-							<Link to="/">Home</Link>
-							<Link to="/about">About</Link>
-							<Link to="/portfolio">Portfolio</Link>
-							<Link to="/contact">Contact</Link>
-					</div>
-				</div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/about">About</Link>
-						</li>
-						<li>
-							<Link to="/portfolio">Portfolio</Link>
-						</li>
-						<li>
-							<Link to="/contact">Contact</Link>
-						</li>
-					</ul>
-				</nav>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/portfolio" element={<Portfolio />} />
-					<Route path="/contact" element={<Contact />} />
-				</Routes>
-			</div>
+			<Layout>
+				<Header>header</Header>
+				<Layout>
+					<Sider>left sidebar</Sider>
+					<Content>
+						<div>
+							<div className="navbar" id={expandNavbar ? "open" : "close"}>
+								<div className="toggleButton">
+									<button onClick={() => {
+										setExpandedNavbar((prev) => !prev); 
+										}}>
+										<MenuOutlined />
+									</button>
+								</div>
+								<div className="links">
+									<Link to="/">Home</Link>
+									<Link to="/about">About</Link>
+									<Link to="/portfolio">Portfolio</Link>
+									<Link to="/contact">Contact</Link>
+								</div>
+							</div>
+						
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/about" element={<About />} />
+								<Route path="/portfolio" element={<Portfolio />} />
+								<Route path="/contact" element={<Contact />} />
+							</Routes>
+						</div>
+				</Content>
+				<Sider>right sidebar</Sider>
+			</Layout>
+			<Footer>footer</Footer>
+			</Layout>
 		</div>
 	);
 }
