fa0fbb713efc9829abca18ada77e0cf7ee02374b
Ivan <ivanma819@gmail.com>
2023-09-24 03:45:07 -0700

    Restore pages and change deployment to gh

---
+++ src/components/Portfolio.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
+import "../styles/Project.css";
+
+const Portfolio = () => {
+	return (
+		<section>
+			<h1>Portfolio</h1>
+			<div className="projectList">
+				{ProjectList.map((project, idx) => {
+					return (
+						<ProjectItem name={project.name} image={project.image} id={idx} />
+					);
+				})}
+			</div>
+		</section>
+	);
+};
+
+export default Portfolio;
