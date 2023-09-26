2aea44e18e1e77d9f3dab7dce0812b05faf1f7d4
Ivan <ivanma819@gmail.com>
2023-09-25 23:44:27 -0700

    Adjust the view settings and contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,25 @@
+import { GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
 import React from "react";
+import "../styles/Contact.css";
 
 const Contact = () => {
 	return (
-		<section>
+		<div className="contact">
 			<h1>Contact</h1>
-			<h2>This is the Contact component!</h2>
-		</section>
+            <h4>My Inbox is open. Message if you want to work together or just to say hi!</h4>
+            <div className="contactItem">
+                <MailFilled></MailFilled>
+			    <div>Email: ivanma9@g.ucla.edu</div>
+            </div>
+			<div className="contactItem">
+                <GithubFilled></GithubFilled>
+                <div>Github: github.com/ivanma9</div>
+            </div>
+			<div className="contactItem">
+				<LinkedinFilled></LinkedinFilled>
+				<div>Linkedin: https://www.linkedin.com/in/ivan-ma9/</div>
+			</div>
+		</div>
 	);
 };
 
